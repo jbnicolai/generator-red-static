73aecb6707d0687e12c47006a534f5c572d7a6f4
Tim Wood <washwithcare@gmail.com>
2013-12-12 16:19:12 -0800

    Using latest grunt-haychtml

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -11,7 +11,6 @@ var CONFIG = {
 
 module.exports = function (grunt) {
 	require('time-grunt')(grunt);
-	require('haychtml')(grunt);
 	require('load-grunt-tasks')(grunt);
 
 	grunt.loadTasks('grunt/tasks');
