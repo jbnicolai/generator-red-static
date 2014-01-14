e035895087c3e3da71a491c178564637fba7ad40
Tim Wood <washwithcare@gmail.com>
2014-01-13 20:22:15 -0800

    Adding html task

---
+++ app/templates/gulp/html.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+	path = require('path'),
+	consolidate = require('gulp-consolidate'),
+	ignore = require('gulp-ignore'),
+	config = require('./config'),
+	minify = require('gulp-minify-html'),
+	prettify = require('gulp-html-prettify');
+
+gulp.task('html-clean', config.clean('**/*.html'));
+
+gulp.task('html', ['html-clean'], function (done) {
+	gulp.src('./pages/**/*.html')
+		.pipe(ignore({
+			pattern : ['**/_**']
+		}))
+		.pipe(consolidate('swig', function (file) {
+			var URL = path.join(path.dirname(file.path), '..');
+			return {
+				STATIC_URL : path.relative(URL, config.src('')) + '/'
+			};
+		}, {
+			usePath : true
+		}))
+		.pipe(minify({
+			conditionals: true
+		}))
+		.pipe(prettify())
+		.pipe(gulp.dest('./deploy'))
+		.on('end', done);
+});
--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,4 +1,5 @@
 require('./gulp/css');
 require('./gulp/font');
+require('./gulp/html');
 require('./gulp/img');
 require('./gulp/js');
