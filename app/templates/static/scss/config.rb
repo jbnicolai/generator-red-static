a5c58c885b5c35624534d149e022fd7875473971
Tim Wood <washwithcare@gmail.com>
2014-02-03 09:36:12 -0800

    Reorganizing config.rb

--- app/templates/static/scss/config.rb
+++ app/templates/static/scss/config.rb
@@ -3,16 +3,14 @@ output_style = :compressed
 relative_assets = true
 
 # Source Directories
-source_dir = 'static/'
-sass_dir        = source_dir + 'scss/'
-images_dir      = source_dir + 'img/'
-javascripts_dir = source_dir + 'js/'
+source_dir              = 'static/'
+sass_dir                = source_dir + 'scss/'
+images_dir              = source_dir + 'img/'
+javascripts_dir         = source_dir + 'js/'
+additional_import_paths = [source_dir + '/lib']
 
 # Destination Directories
-dest_dir = 'deploy/static/'
+dest_dir             = 'deploy/static/'
 css_dir              = dest_dir + 'css/'
 fonts_dir            = dest_dir + 'fonts/'
 generated_images_dir = dest_dir + 'img/'
-
-# Include Lib Directory
-add_import_path 'static/lib'
