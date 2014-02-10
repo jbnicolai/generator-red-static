f871135a28d0f5b47fe82cf7710003369341c470
Tim Wood <washwithcare@gmail.com>
2014-02-10 09:19:00 -0800

    Fixing a typo on the libs.js watcher

--- app/templates/grunt/config/watch.js
+++ app/templates/grunt/config/watch.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
 			tasks: ['neuter:app']
 		},
 		neuterLibs: {
-			files: [config.source + 'js/libs.js}'],
+			files: [config.source + 'js/libs.js'],
 			tasks: ['neuter:libsDevelop']
 		},
 		copy : {
