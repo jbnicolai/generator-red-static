14b02af74e2cbe703f9e6823faf88ab061102ff9
Tim Wood <washwithcare@gmail.com>
2014-06-06 12:02:37 -0700

    Add handlebars helpers and move scss mixins to a more appropriately named 'helpers' folder.

--- app/templates/static/js/app.js
+++ app/templates/static/js/app.js
@@ -3,5 +3,6 @@ var App = window.App = Ember.Application.create();
 require('js/mixins/*');
 require('js/models/*');
 require('js/components/*');
+require('js/helpers/*');
 require('js/apps/*/*');
 require('js/router');
